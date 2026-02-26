42ab46ee12fa8c716b4a2b62cca628d214771da3
YoungDev <youngdev@Depas-MacBook-Air.local>
2026-02-26 18:10:43 +0700

    คืนค่า

--- src/App.tsx
+++ src/App.tsx
@@ -308,7 +308,7 @@ const Footer = () => {
 // --- Page Components ---
 
 const HomePage = () => {
- return (
+  return (
     <div>
       <HeroSlider />
       <section className="py-24 bg-stone-100 overflow-hidden">
@@ -386,7 +386,7 @@ const AboutPage = () => {
             >
               <div className="absolute inset-0 bg-emerald-500 transform -translate-x-4 translate-y-4 md:-translate-x-6 md:translate-y-6 rounded-3xl" />
               <img 
-                src="https://picsum.photos/seed/herb-extract/800/800" 
+                src="https://drive.google.com/file/d/1zFeeSkMTPWX_NhD8zWu-icYRyUeEW8Ye/view?usp=sharing" 
                 alt="Herbal Extract" 
                 className="relative z-10 w-full aspect-square object-cover rounded-3xl shadow-2xl border-4 border-white"
                 referrerPolicy="no-referrer"
