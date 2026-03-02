1b10741c5a49b7fd8967bfd2d9510bacd8fd5231
YoungDev <youngdev@Depas-MacBook-Air.local>
2026-03-03 00:45:00 +0700

    แก้รูป

--- src/App.tsx
+++ src/App.tsx
@@ -323,7 +323,7 @@ const HomePage = () => {
             >
               {/* เปลี่ยน URL รูปภาพโปรโมทน้ำหมักที่ src ด้านล่าง */}
               <img 
-                src="https://drive.google.com/file/d/1DvATlWU-wVt4t4dfalHhQ_-Ra-VXe9vW/view?usp=sharing" 
+                src="https://drive.google.com/file/d/1DvATlWU-wVt4t4dfalHhQ_-Ra-VXe9vW/view" 
                 alt="รูปภาพโปรโมทน้ำหมัก" 
                 className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
                 referrerPolicy="no-referrer"
