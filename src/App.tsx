33ef07116f391bc56efbb98a620c8930d44f7e15
YoungDev <youngdev@Depas-MacBook-Air.local>
2026-03-03 01:42:09 +0700

    D

--- src/App.tsx
+++ src/App.tsx
@@ -323,7 +323,7 @@ const HomePage = () => {
             >
               {/* เปลี่ยน URL รูปภาพโปรโมทน้ำหมักที่ src ด้านล่าง */}
               <img 
-                src="/images/product_1.jpg" 
+                src="/images/product_.jpg" 
                 alt="รูปภาพโปรโมทน้ำหมัก" 
                 className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
                 referrerPolicy="no-referrer"
